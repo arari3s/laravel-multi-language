27b9fde4a46dfdf9e0831e2ab547f76e0d1e4724
arari3s <arlanariandi@gmail.com>
2023-02-07 22:58:12 +0700

    UPDATE: Update navbar Add lang en and id Add route view articles, about and contact

--- routes/web.php
+++ routes/web.php
@@ -3,20 +3,13 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::view('articles', 'articles')->name('articles');
+Route::view('about', 'about')->name('about');
+Route::view('contact', 'contact')->name('contact');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -28,4 +21,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
